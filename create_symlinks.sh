13c4fef65fa6067b273be89c527104e27c8b0a25
Pieter Brandwijk <pieter@pbrandwijk.com>
2023-06-08 00:33:04 +0200

    Update symlinks

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 echo -e "so ~/dotfiles/.vimrc\n" > ~/.vimrc
+echo -e "source ~/dotfiles/.zshrc\n" > ~/.zshrc
+
+ln -s -T ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s -T ~/dotfiles/.aliases ~/.aliases
 
