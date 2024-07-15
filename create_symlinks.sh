f57c86e3e819177e07c435d28f14d280be89ad3c
Pieter Brandwijk <pieter@pbrandwijk.com>
2024-07-15 11:50:47 +0200

    Add symlink for Kitty config

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -6,4 +6,4 @@ echo -e "source ~/dotfiles/.zshrc\n" > ~/.zshrc
 ln -s -T ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s -T ~/dotfiles/.aliases ~/.aliases
 ln -s -T ~/dotfiles/.gemrc ~/.gemrc
-
+ln -s -T ~/dotfiles/kitty-config ~/.config/kitty
