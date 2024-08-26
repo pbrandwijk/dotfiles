395b3ac0cc5979b52e3be7b92eec720397f92206
Pieter Brandwijk <pieter@pbrandwijk.com>
2024-08-26 22:54:09 +0200

    Add spelling languages

--- lazyvim-config/lua/config/options.lua
+++ lazyvim-config/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- PB: English, Dutch and Spanish spelling
+vim.opt.spelllang = {"en", "nl", "es"}
