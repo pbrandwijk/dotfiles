1e833f11e094a6eaa47eb47ca8bbb3bf0a335039
Pieter Brandwijk <pieter@pbrandwijk.com>
2024-08-28 11:28:08 +0200

    Add legder config

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -6,5 +6,6 @@ echo -e "source ~/dotfiles/.zshrc\n" > ~/.zshrc
 ln -s -T ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s -T ~/dotfiles/.aliases ~/.aliases
 ln -s -T ~/dotfiles/.gemrc ~/.gemrc
+ln -s -T ~/dotfiles/.ledgerrc ~/.ledgerrc
 ln -s -T ~/dotfiles/kitty-config ~/.config/kitty
 ln -s -T ~/dotfiles/lazyvim-config ~/.config/nvim
