cd6ff07a0343cb8483709b98ab0c3ed72a898e92
Pieter Brandwijk <pieter@pbrandwijk.com>
2024-08-29 09:39:58 +0200

    Add key mappings

--- lazyvim-config/lua/config/keymaps.lua
+++ lazyvim-config/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map "hh" key sequence to <Esc> in insert mode
+vim.keymap.set('i', 'hh', '<Esc>')
+
+-- Easier window navigation
+vim.keymap.set('n', '<C-H>', '<C-W>h')
+vim.keymap.set('n', '<C-T>', '<C-W>j')
+vim.keymap.set('n', '<C-N>', '<C-W>k')
+vim.keymap.set('n', '<C-S>', '<C-W>l')
+
+-- Easy access to beginning and end of line
+vim.keymap.set('n', '-', '$')
+vim.keymap.set('n', '_', '^')
+
+-- Obsidian notes
+vim.keymap.set('n', 'md', '<cmd>ObsidianToday<cr>')
+vim.keymap.set('n', 'mt', '<cmd>ObsidianTemplate<cr>')
