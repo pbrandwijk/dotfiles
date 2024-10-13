99ffa7a628ce767dd043784476b351024a6196ff
Pieter Brandwijk <pieter@pbrandwijk.com>
2024-10-13 17:53:57 +0200

    Add atuin config

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -9,3 +9,4 @@ ln -s -T ~/dotfiles/.gemrc ~/.gemrc
 ln -s -T ~/dotfiles/.ledgerrc ~/.ledgerrc
 ln -s -T ~/dotfiles/kitty-config ~/.config/kitty
 ln -s -T ~/dotfiles/lazyvim-config ~/.config/nvim
+ln -s -T ~/dotfiles/atuin-config ~/.config/atuin
