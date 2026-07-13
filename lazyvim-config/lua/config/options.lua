b196ea8a19439b8f1dbcdab2fdd0afa759b24097
Pieter Brandwijk <pieter@pbrandwijk.com>
2026-07-13 16:17:53 +0200

    Update tab settings to match VScode

--- lazyvim-config/lua/config/options.lua
+++ lazyvim-config/lua/config/options.lua
@@ -8,8 +8,12 @@ vim.opt.spelllang = {"en", "nl", "es"}
 -- Turn off smooth scrolling
 vim.g.snacks_animate = false
 
--- Set tab width to 4
+-- Set tab width to 4 spaces and do not expand. This matches
+-- the VS Code Javascript formatter config
+vim.o.expandtab = false
 vim.o.tabstop = 4
-vim.bo.tabstop = 4
 vim.o.shiftwidth = 4
-vim.bo.shiftwidth = 4
+vim.o.softtabstop = 4
+
+-- Turn on automattic formatting by ESLint
+--vim.g.lazyvim_eslint_auto_format = true
